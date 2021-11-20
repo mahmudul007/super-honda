7ece556871ad59c82770c0295339456ab0c267c3
mahmudul007 <62248559+mahmudul007@users.noreply.github.com>
2021-11-20 15:04:57 +0600

    init

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,15 +1,9 @@
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_APP_ID,
-    apiKey: "AIzaSyBcqK61nF7cQ_CrNn_2OpOeKh-r0tpxq4I",
-    authDomain: "honda-project-1f7f5.firebaseapp.com",
-    projectId: "honda-project-1f7f5",
-    storageBucket: "honda-project-1f7f5.appspot.com",
-    messagingSenderId: "885704596541",
-    appId: "1:885704596541:web:3e57fcd99f31c3b3649e54"
+    apiKey: "AIzaSyCu5nCHdvamn56klHnbq2K-ubzqkANffO8",
+    authDomain: "honda-project-634ee.firebaseapp.com",
+    projectId: "honda-project-634ee",
+    storageBucket: "honda-project-634ee.appspot.com",
+    messagingSenderId: "587975771166",
+    appId: "1:587975771166:web:dd00f236e1417f28341a0f"
 };
 export default firebaseConfig;
